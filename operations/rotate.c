0a9e82c423695044d98bc0c795ddc923a8f35148
Judith Camille Rosette <167699879+helicohedron@users.noreply.github.com.>
2026-09-17 17:07:04 +0200

    add general rotate function, add ra, rb, rr

--- operations/rotate.c
+++ operations/rotate.c
@@ -1,5 +1,47 @@
 #include "../push_swap.h"
 
-void	ra(t_node **a);
-void	rb(t_node **b);
-void	rr(t_node **a, t_node **b);
+static	t_node *lastnode(t_node *x)
+{
+	if (!x)
+		return (NULL);
+	while (x->next != NULL)
+		x = x->next;
+	return (x);
+}
+
+void    rotate(t_node **x)
+{
+	t_node	*temp;
+	t_node	*last;
+	// handle if x is null
+	if (*x == NULL)
+		return ;
+	// handle if x-> next is null (just 1 node)
+	if ((*x)->next == NULL)
+		return ;
+	// save first node pointed by x to temp
+	temp = *x;
+	// move x pointer to 2nd node
+	*x = (*x)->next;
+	// detach first node of temp
+	temp->next = NULL;
+	// attach temp to end of x
+	last = lastnode(*x);
+	last->next = temp;
+}
+
+void	ra(t_node **a)
+{
+	rotate(a);
+}
+
+void	rb(t_node **b)
+{
+	rotate(b);
+}
+
+void	rr(t_node **a, t_node **b)
+{
+	rotate(a);
+	rotate(b);
+}
