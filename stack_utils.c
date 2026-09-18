febe1bc1a69c20216fdfd85a28dc2ce0ded16b37
Judith Camille Rosette <167699879+helicohedron@users.noreply.github.com.>
2026-09-18 21:47:27 +0200

    add find_min in stack utils

--- stack_utils.c
+++ stack_utils.c
@@ -2,12 +2,16 @@
 
 Helper functions for building, inspecting, and cleaning up the linked-list stack. These may include:
 
-1. new_node: creates a new stack || t_node	*new_node(int value);
-2. stack_add_back: adds a new node at the end || void stack_add_back(t_node **a, t_node *new);
-3. stack_size: returns the number of nodes || int	stack_size(t_node *a);
-4. is_sorted: checks if the stack is sorted || int     is_sorted(t_node *a);
-5. last_node: returns a pointer to the last node (see rotate.c)
-6. free_stack: destroys the stack || void    free_stack(t_node **a);
+is_sorted: checks if the stack is sorted || int     is_sorted(t_node *a);
+last_node: returns a pointer to the last node
+find_min: returns a pointer to the node with the smallest value
+
+
+To do:
+new_node: creates a new stack || t_node	*new_node(int value);
+stack_add_back: adds a new node at the end || void stack_add_back(t_node **a, t_node *new);
+stack_size: returns the number of nodes || int	stack_size(t_node *a);
+free_stack: destroys the stack || void    free_stack(t_node **a);
 
 */
 
@@ -34,4 +38,20 @@ t_node	*lastnode(t_node *a)
 	while (a->next != NULL)
 		a = a->next;
 	return (a);
+}
+
+t_node	*find_min(t_node *a)
+{
+	t_node	*min;
+
+	if (!a)
+		return (NULL);
+	min = a;
+	while (a->next != NULL)
+	{
+		if (a->next->value < min->value)
+			min = a->next;
+		a = a->next;
+	}
+	return (min);
 }
