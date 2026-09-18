d41a3e7a35d0681381e75366f69a3a63d5715959
Judith Camille Rosette <167699879+helicohedron@users.noreply.github.com.>
2026-09-18 16:43:07 +0200

    add sort_three

--- algorithms/small.c
+++ algorithms/small.c
@@ -3,15 +3,48 @@ Specialized handling for n<= 5
 */
 #include "../push_swap.h"
 
-void    sort_two(t_node **a)
+void	sort_two(t_node **a)
 {
-    int sorted;
-
-    sorted = is_sorted(*a);
-    if (sorted != 0)
-        sa(a);
+	if (is_sorted(*a) != 0)
+		sa(a);
 }
 
-void    sort_three(t_node **a);
+void	sort_three(t_node **a)
+{
+	if (is_sorted(*a) != 0)
+		sa(a);
+	// Largest at the bottom: 213 -> sa
+	if ((*a)->next->next->value > (*a)->next->value &&
+		(*a)->next->next->value > (*a)->value)
+			sa(a);
+	// Largest at the middle
+	if ((*a)->next->value > (*a)->value &&
+		(*a)->next->value > (*a)->next->next->value)
+	{
+		// 231 -> rra
+		if ((*a)->value > (*a)->next->next->value)
+			rra(a);
+		// 132 -> sa, ra
+		if ((*a)->value < (*a)->next->next->value)
+		{
+			sa(a);
+			ra(a);
+		}
+	}
+	// Largest at the top
+	if ((*a)->value >  (*a)->next->value &&
+		(*a)->value > (*a)->next->next->value)
+	{
+		// 312 -> ra
+		if ((*a)->next->value < (*a)->next->next->value)
+			ra(a);
+		// 321 -> sa, rra
+		if ((*a)->next->value > (*a)->next->next->value)
+		{
+			sa(a);
+			rra(a);
+		}
+	}
+}
 void    sort_four(t_node **a, t_node **b);
 void    sort_five(t_node **a, t_node **b);
