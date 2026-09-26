0e0c4894f4a95eeff991a644687f90c84af793bb
Judith Camille Rosette <jrosette@c2r9s6.42prague.com>
2026-09-26 13:52:05 +0200

    add static find_length function

--- input/parse.c
+++ input/parse.c
@@ -6,11 +6,51 @@
 /*   By: jrosette <jrosette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/23 12:45:32 by jrosette          #+#    #+#             */
-/*   Updated: 2026/09/26 13:30:59 by jrosette         ###   ########.fr       */
+/*   Updated: 2026/09/26 13:50:50 by jrosette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+static int	is_valid_string(char *input);
+static char	**add_to_array(char **array, char *string);
+static int	count_numbers(char *string);
+
+char	**parse(int argc, char **argv)
+{
+	int		i;
+	char	**array;
+	int		total;
+
+	if (argc == 1)
+		return (NULL);
+	i = 1;
+	if (!argv[i])
+		return (NULL);
+	array = NULL;
+	total = 0;
+	while (argv[i])
+	{
+		if (!is_valid_string(argv[i]))
+			return (0);
+		total += count_numbers(argv[i]);
+		i++;
+	}
+	array = malloc((total + 1) * sizeof(char *));
+	i = 0;
+	while (i <= total)
+	{
+		array[i] = NULL;
+		i++;
+	}	
+	i = 1;
+	while (argv[i])
+	{
+		array = add_to_array(array, argv[i]);
+		i++;
+	}
+	return (array);
+}
 
 static int	is_valid_string(char *input)
 {
@@ -39,24 +79,23 @@ static int	is_valid_string(char *input)
 	return (1);
 }
 
-/*SHORTEN FUNCTION -- maybe copy_number*/
-static char	**add_to_array(char **array, char *string, int i)
+static char	**add_to_array(char **array, char *string)
 {
 	int	start;
 	int	end;
+	int	i;
 	int	j;
 	int	length;
 
 	start = 0;
+	i = 0;
+	while (array[i])
+		i++;
 	while (string[start])
 	{
-		while (is_whitespace(string[start]))
-			start++;
-		end = start;
-		while (is_digit(string[end]) || is_sign(string[end]))
-			end++;
-		length = end - start;
+		length = find_length(string, start);
 		array[i] = malloc(length + 1);
+		end = start + length;
 		j = 0;
 		while (start < end)
 		{
@@ -90,38 +129,14 @@ static int	count_numbers(char *string)
 	return (count);
 }
 
-char	**parse(int argc, char **argv)
+static int	find_length(char *string, int start)
 {
-	int		i;
-	char	**array;
-	int		total;
+	int	end;
 
-	if (argc == 1)
-		return (NULL);
-	i = 1;
-	if (!argv[i])
-		return (NULL);
-	array = NULL;
-	total = 0;
-	while (argv[i])
-	{
-		if (!is_valid_string(argv[i]))
-			return (0);
-		total += count_numbers(argv[i]);
-		i++;
-	}
-	array = malloc((total + 1) * sizeof(char *));
-	i = 0;
-	while (i <= total)
-	{
-		array[i] = NULL;
-		i++;
-	}	
-	i = 1;
-	while (argv[i])
-	{
-		array = add_to_array(array, argv[i], i);
-		i++;
-	}
-	return (array);
+	while (is_whitespace(string[start]))
+		start++;
+	end = start;
+	while (is_digit(string[end]) || is_sign(string[end]))
+		end++;
+	return (end - start);
 }
